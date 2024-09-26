fa73927d11c06023187d680949697e6d817c30dc
alissonwr <alisson-wendel@hotmail.com>
2024-09-25 22:05:21 -0300

    Adicionando o pacote package_name_Alisson

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name_Alisson",
+    name="package_name_Alisson_Wendel",
     version="0.0.1",
     author="Alisson",
     description="Pacote para estudo de conceitos de criação de pacotes",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="",
+    url="https://github.com/alissonwr/simple-package-template.git",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.5',
